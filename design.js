39bde7df7dfb9c3ae3dd5b9ef49596ca0ce49c22
Koustav Chowdhury <kc99.kol@gmail.com>
2019-03-18 02:02:16 +0530

    Update design.js

--- design.js
+++ design.js
@@ -2,7 +2,7 @@ const slides = document.querySelectorAll('.slider-container .slide');
 const eraser = document.querySelector('.eraser');
 const prev = document.getElementById('previous');
 const next = document.getElementById('next');
-const intervalTime = 8000;
+const intervalTime = 7000;
 const eraserActiveTime = 700;
 let sliderInterval;
 
@@ -54,4 +54,36 @@ prev.addEventListener('click', () => {
 sliderInterval = setInterval(nextSlide, intervalTime);
 
 // Initial slide
-setTimeout(nextSlide, 2000);
+setTimeout(nextSlide, 100);
+
+
+window.fbAsyncInit = function() {
+	FB.init({
+	  appId            : '2070778906293269',
+	  autoLogAppEvents : true,
+	  xfbml            : true,
+	  version          : 'v3.2'
+	});
+	};
+	function login() {
+	FB.login(function(response) {
+		if (response.status === 'connected') {
+			document.getElementById('status').innerHTML = 'We are connected.';
+			document.getElementById('login').style.visibility = 'hidden';
+		} else if (response.status === 'not_authorized') {
+			document.getElementById('status').innerHTML = 'We are not logged in.'
+		} else {
+			document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
+		}
+	}, {scope: 'email'});
+	FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id'}, function(response) {
+		document.getElementById('yname').innerHTML = '<b>' + response.first_name + '</b>';
+	});
+	}
+
+	function getInfo() {
+	FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id'}, function(response) {
+		document.getElementById('status').innerHTML = response.id;
+	});
+	}
+
