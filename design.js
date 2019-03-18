ffa02771a02da9c783f31b2e63c9db28ff14e4dc
Koustav Chowdhury <kc99.kol@gmail.com>
2019-03-18 07:41:48 +0530

    Update design.js

--- design.js
+++ design.js
@@ -66,28 +66,7 @@ login.addEventListener('click', () => {
 	  version          : 'v3.2'
 	});
 	console.log(FB);
-	};/*
-	// (function(d, s, id){
- //      var js, fjs = d.getElementsByTagName(s)[0];
- //      if (d.getElementById(id)) {return;}
- //      js = d.createElement(s); js.id = id;
- //      js.src = "//connect.facebook.net/en_US/sdk.js";
- //      fjs.parentNode.insertBefore(js, fjs);
- //    }(document, 'script', 'facebook-jssdk'));
-	// $(window).FB.login(function(response) {
-	// 	console.log(response);
-	// 	if (response.status === 'connected') {
-	// 		document.getElementById('status').innerHTML = 'We are connected.';
-	// 		document.getElementById('login').style.visibility = 'hidden';
-	// 	} else if (response.status === 'not_authorized') {
-	// 		document.getElementById('status').innerHTML = 'We are not logged in.'
-	// 	} else {
-	// 		document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
-	// 	}
-	// }, {scope: 'email'});
-	// $(window).FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id'}, function(response) {
-	// 	document.getElementById('yname').innerHTML = '<b>' + response.first_name + '</b>';
-	// });*/
+	};
 });
 
 
