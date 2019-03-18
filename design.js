caf99716b061e898a1639281d72caa1af6d7b811
Koustav Chowdhury <kc99.kol@gmail.com>
2019-03-18 08:19:31 +0530

    Update design.js

--- design.js
+++ design.js
@@ -65,9 +65,16 @@ login.addEventListener('click', () => {
 	  xfbml            : true,
 	  version          : 'v3.2'
 	});
-	FB.login();
+	FB.login(function(response) {
+    if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+    } else {
+     console.log('User cancelled login or did not fully authorize.');
+    }
+});
 	console.log(FB);
 	
 });
-
-
